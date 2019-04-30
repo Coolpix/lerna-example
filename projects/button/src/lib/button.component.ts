903b2ab9a4b9ef06996e75fe7d7b70a4944e5623
sergio alonso <sergio.alonso@atmira.com>
2019-04-30 16:37:07 +0200

    feat(Button component): Cambios en el DOM del componente
    Se ha cambiado el DOM que genera el componente, se ha pasado de un <p> a un <button>
    
    BREAKING CHANGE: Al cambiar el DOM generador, todas las aplicaciones que usen este componente
    deberán adaptarse a dicho cambio.

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-button',
   template: `
-    <p>
+    <button>
       Button works!
-    </p>
+    </button>
   `,
   styles: []
 })
